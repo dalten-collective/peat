1e01f39746ca11036805a36f438a5444bed3648f
vinney cavallo <vcavallo@gmail.com>
2022-06-02 16:26:51 -0400

    Add interface and json mark

---
+++ interface/src/components/Start.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Hello world</h1>
+  This is Start.vue
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  mounted() {
+    const deskname = "peat"
+    this.startAirlock(deskname);
+  },
+  unmounted() {
+    this.closeAirlocks();
+  },
+
+  methods: {
+    startAirlock(deskname) {
+      this.$store.dispatch("ship/openAirlockToAgent", deskname);
+    },
+    closeAirlocks() {
+      this.$store.dispatch("ship/closeAgentAirlocks");
+    },
+  },
+})
+</script>
+
+
