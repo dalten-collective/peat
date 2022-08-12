cb37c6e63ecedc47f5db3d0f9dbaf1a58820d8ad
vinney cavallo <vcavallo@gmail.com>
2022-08-12 15:26:24 -0400

    Add basic about

--- interface/src/views/Learn.vue
+++ interface/src/views/Learn.vue
@@ -1,15 +1,35 @@
 <template>
-  <v-card class="tw-grow tw-p-4 tw-bg-white tw-border tw-border-secondary tw-rounded">
+  <v-card
+    class="tw-grow tw-p-4 tw-bg-white tw-border tw-border-secondary tw-rounded"
+  >
     <div class="tw-flex tw-flex-row tw-my-8">
       <v-card-text>
-        <div class="tw-flex tw-flex-col">
-          <h1 class="tw-text-2xl tw-mb-4">About</h1>
+        <div class="tw-flex tw-flex-row tw-items-center tw-justify-center">
 
-          <div class="tw-flex">
-            <div>
-              <p>Things here</p>
+          <div class="tw-w-full"></div>
+
+          <div class="tw-flex tw-flex-col">
+            <h1 class="tw-text-2xl tw-mb-4">About</h1>
+
+            <div class="tw-flex tw-flex-row">
+              <div class="tw-w-1/2">
+                <p>
+                  Peat is a response to a community demand for backing up and
+                  restoring social graphs on Urbit. Peat comes as the culmination
+                  of a long line of products working against <span class="tw-font-mono">%graph-store</span>. The
+                  technology underlying Peat was first built at the turn of
+                  2021-2022 to help restore Dalten Collective's chat and was, at
+                  the time, called <span class="tw-font-mono">%oger</span>. Peat is vastly superior to our prior
+                  effort in <span class="tw-font-mono">%oger</span>, and we think you'll like it.
+                </p>
+
+                <p class="tw-mt-4">With love,</p>
+
+                <p><span class="tw-font-bold">Quartus</span></p>
+              </div>
             </div>
           </div>
+
         </div>
       </v-card-text>
     </div>
@@ -17,7 +37,5 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
