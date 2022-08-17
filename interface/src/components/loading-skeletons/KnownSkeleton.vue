1cdfb6d6fb7b69700f252117c168172dc23bfdd9
vinney cavallo <vcavallo@gmail.com>
2022-08-17 14:04:45 -0400

    Update known skeleton loader for new layout

--- interface/src/components/loading-skeletons/KnownSkeleton.vue
+++ interface/src/components/loading-skeletons/KnownSkeleton.vue
@@ -5,16 +5,13 @@
     <div class="tw-flex tw-animate-pulse">
       <div class="tw-flex-1 tw-py-1 tw-space-y-6">
         <div class="tw-space-y-3">
-          <div class="tw-grid tw-grid-cols-4 tw-gap-4">
-            <div class="tw-h-2 tw-rounded tw-bg-primary tw-col-span-1"></div>
+          <div class="tw-grid tw-grid-cols-9 tw-gap-1">
+            <div class="tw-h-2 tw-rounded tw-bg-primary tw-col-span-2"></div>
+            <div class="tw-h-2 tw-rounded tw-bg-white tw-col-span-4"></div>
             <div class="tw-h-2 tw-rounded tw-bg-info tw-col-span-1"></div>
-            <div class="tw-h-2 tw-rounded tw-bg-secondary tw-col-span-2"></div>
+            <div class="tw-h-2 tw-rounded tw-bg-info tw-col-span-1"></div>
+            <div class="tw-h-2 tw-rounded tw-bg-primary tw-col-span-1"></div>
           </div>
-          <div class="tw-h-2 tw-rounded tw-bg-secondary"></div>
-          <div class="tw-h-2 tw-rounded tw-bg-secondary"></div>
-        </div>
-        <div class="tw-grid tw-grid-cols-6">
-          <div class="tw-h-2 tw-rounded tw-bg-success tw-col-span-1"></div>
         </div>
       </div>
     </div>
